21dde40ec67a10543137fe7b8cfd906f899ea59a
Fon Nkwent <fonnkwenti85@gmail.com>
2022-08-04 11:39:16 +0100

    cognito user pools

--- NOTES-CRUD-API/notes/authorizer.js
+++ NOTES-CRUD-API/notes/authorizer.js
@@ -1,10 +1,16 @@
 const { CognitoJwtVerifier } = require("aws-jwt-verify"); // JWT verification library from amazon for cognito. We are deconstructing the CognitoJwtVerifier method to use
 // the libary will check if the token is generated from our user pool and if the token is expired and it will support user pool groups and also handle error.
 
+const userPoolId = process.env.COGNITO_USERPOOL_ID;
+const clientId = process.env.COGNITO_WEB_CLIENT_ID;
+
 const jwtVerifier = CognitoJwtVerifier.create({
-  userPoolId: "us-east-1_cilW6Plk9", // copy the user pool ID from the Cognito user pool console
   tokenUse: "id", // we will use id token in place of access token because id token has some information about the user
-  clientId: "1lgthkk5tclh7f901iu7lhotqm", // get the client ID from the cognito console
+  // userPoolId: "us-east-1_cilW6Plk9", // copy the user pool ID from the Cognito user pool console
+  // clientId: "1lgthkk5tclh7f901iu7lhotqm", // get the client ID from the cognito console
+
+  userPoolId, // replacing the hard-coded user pool with that gotten from SLS environment variables
+  clientId, // replacing the hard-coded user pool with that gotten from SLS environment variables
 });
 
 const generatePolicy = (principalId, effect, resource) => {
@@ -34,20 +40,18 @@ const generatePolicy = (principalId, effect, resource) => {
   return authResponse;
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   // lambda authorizer code
 
   const token = event.authorizationToken; // we have to pass the token in the authorizer header. we access it using the event.authorizationToken and assigning to the token variable
-  console.log(token)
+  console.log(token);
   try {
-    const payload = await jwtVerifier.verify(token)
-    console.log(JSON.stringify(payload))
-    callback(null, generatePolicy("user","Allow","event.methodArn")) // generate the policy and return to the API Gateway
-    
+    const payload = await jwtVerifier.verify(token);
+    console.log(JSON.stringify(payload));
+    callback(null, generatePolicy("user", "Allow", event.methodArn)); // generate the policy and return to the API Gateway
   } catch (error) {
-    console.error(error)
-    callback("Error: Invalid token")
-    
+    console.error(error);
+    callback("Error: Invalid token");
   }
 
   /*
